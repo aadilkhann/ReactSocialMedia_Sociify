7fc9b8c533fb90efd9dfa642535d98e7f65b5fe8
Adil Khan <khan01970@gmail.com>
2023-06-29 20:31:14 +0530

    Full Project

---
+++ api/controllers/relationship.js
@@ -0,0 +1,48 @@
+import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+
+export const getRelationships = (req,res)=>{
+    const q = "SELECT followeruserId FROM relationships WHERE followinguserId = ?";
+
+    db.query(q, [req.query.followinguserId], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json(data.map(relationship=>relationship.followeruserId));
+    });
+}
+
+export const addRelationship = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q = "INSERT INTO relationships (`followeruserId`,`followinguserId`) VALUES (?)";
+    const values = [
+      userInfo.id,
+      req.body.userId
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Following");
+    });
+  });
+};
+
+export const deleteRelationship = (req, res) => {
+
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q = "DELETE FROM relationships WHERE `followeruserId` = ? AND `followinguserId` = ?";
+
+    db.query(q, [userInfo.id, req.query.userId], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Unfollow");
+    });
+  });
+};
---
+++ api/controllers/story.js
@@ -0,0 +1,63 @@
+import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+import moment from "moment";
+
+export const getStories = (req, res) => {
+  const userId = req.query.userId;
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    console.log(userId);
+
+    const q = `SELECT s.*, name FROM stories AS s JOIN users AS u ON (u.id = s.userId)
+    LEFT JOIN relationships AS r ON (s.userId = r.followinguserId AND r.followeruserId= ?) ORDER BY s.time DESC LIMIT 4`;
+
+    db.query(q, [userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json(data);
+    });
+  });
+};
+
+export const addStory = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q = "INSERT INTO stories(`img`, `userId`, `time`) VALUES (?)";
+    const values = [
+      req.body.img,
+      userInfo.id,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+     
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Story has been created.");
+    });
+  });
+};
+
+export const deleteStory = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q = "DELETE FROM stories WHERE `id`=? AND `userId` = ?";
+
+    db.query(q, [req.params.id, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if (data.affectedRows > 0)
+        return res.status(200).json("Story has been deleted.");
+      return res.status(403).json("You can delete only your story!");
+    });
+  });
+};
